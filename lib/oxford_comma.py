82cd69eadf0c904ca53a74812a3d2a428456713f
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-27 12:18:09 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return " and ".join(items)
+    items[-1] = "and " + items[-1]
+    return ", ".join(items)
+
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
